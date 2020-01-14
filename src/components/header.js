47090968c5dbedef41b1d782727261c6cbda8be7
maximedupre <maximedupre@me.com>
2020-01-14 15:57:37 -0500

    cleans up the project a bit

--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,9 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
