7a71b68f2f5eab1e6c31a408453b5e413d9d17d1
maximedupre <maximedupre@me.com>
2020-01-16 22:46:12 -0500

    Changes typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
-import fairyGateTheme from 'typography-theme-fairy-gates'
+import noriegaTheme from 'typography-theme-noriega'
 
-const typography = new Typography(fairyGateTheme)
+const typography = new Typography(noriegaTheme)
 
 export const { scale, rhythm, options } = typography
 export default typography
