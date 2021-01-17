69eff8bb6e082f87e2cfe010362b8ddb19edce84
Maxime Dupré <maximedupre@me.com>
2021-01-17 17:59:56 -0500

    add navbar

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: {
-        tailwindcss: { purge: ['./index.html'] },
+        tailwindcss: {},
         autoprefixer: {},
     },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    purge: ['./index.html'],
+    theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+        },
+    },
+};
