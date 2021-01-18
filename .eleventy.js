02ea7959c253be20dfc641bafee1a6c0e200e792
Maxime Dupré <maximedupre@me.com>
2021-01-17 19:09:01 -0500

    added content

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,22 @@
+const path = require('path');
+const Image = require('@11ty/eleventy-img');
+
+(async () => {
+    for (let name of ['skytracks', 'holyclony', 'maximedupre']) {
+        await Image('src/images/' + name + '.png', {
+            widths: [300],
+            formats: ['webp'],
+            outputDir: 'dist/images',
+            filenameFormat: function (id, src, width, format, options) {
+                const extension = path.extname(src);
+                const name = path.basename(src, extension);
+
+                return `${name}.${format}`;
+            },
+        });
+    }
+})();
+
 module.exports = {
     dir: {
         input: 'src',
