f4b1d39f12bee5079d52517533ef65a34bf8760c
Maxime Dupré <maximedupre@me.com>
2021-01-18 15:26:48 -0500

    adds a color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,14 @@ module.exports = {
             center: true,
             padding: '2rem',
         },
+        colors: {
+            black: 'black',
+            white: 'white',
+            1: '#495867',
+            2: '#577399',
+            3: '#bdd5ea',
+            4: '#f7f7ff',
+            5: '#fe5f55',
+        },
     },
 };
