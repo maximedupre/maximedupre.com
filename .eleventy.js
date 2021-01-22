2db9e2b6cd6515cd1fe71d6bf1ce162491c08b8b
Maxime Dupré <maximedupre@me.com>
2021-01-22 15:42:27 -0500

    adds a facivon

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,13 @@ const Image = require('@11ty/eleventy-img');
 
 (async () => {
     for (let i of [
-        { name: 'skytracks', ext: 'png' },
-        { name: 'holyclony', ext: 'png' },
-        { name: 'maximedupre', ext: 'png' },
+        { name: 'skytracks', ext: 'png', width: 300 },
+        { name: 'holyclony', ext: 'png', width: 300 },
+        { name: 'maximedupre', ext: 'png', width: 300 },
+        { name: 'favicon', ext: 'png', width: 100 },
     ]) {
         await Image('src/images/' + i.name + '.' + i.ext, {
-            widths: [300],
+            widths: [i.width],
             formats: ['webp'],
             outputDir: 'dist/images',
             filenameFormat: function (id, src, width, format, options) {
